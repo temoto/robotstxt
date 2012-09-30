5fc387e4b8cb1e77b0a9c936c81ae9ea8376b52e
Martin Angers <martin.n.angers@gmail.com>
2012-09-29 21:48:20 -0400

    fix numbered list

--- parser.go
+++ parser.go
@@ -1,96 +1,129 @@
 package robotstxt
 
 import (
-    "errors"
-    "io"
-    "strconv"
-    "strings"
+	"errors"
+	"io"
+	"strconv"
+	"strings"
 )
 
-type Parser struct {
-    tokens []string
-    pos    int
-    agent  string
+type parser struct {
+	tokens   []string
+	pos      int
+	agent    string
+	sitemaps []string
 }
 
 func NewParser(tokens []string) *Parser {
-    return &Parser{tokens: tokens}
+	return &Parser{tokens: tokens}
 }
 
 func (p *Parser) ParseAll() (result []Rule, err error) {
-    var r *Rule
-    err = nil
-    for {
-        r, err = p.ParseRule()
-        if r != nil {
-            result = append(result, *r)
-        }
-        if err == io.EOF {
-            err = nil
-            break
-        }
-    }
-    return result, err
+	var r *Rule
+	err = nil
+	for {
+		r, err = p.ParseRule()
+		if r != nil {
+			result = append(result, *r)
+		}
+		if err == io.EOF {
+			err = nil
+			break
+		}
+	}
+	return result, err
 }
 
 func (p *Parser) ParseRule() (r *Rule, err error) {
-    t1, ok1 := p.popToken()
-    if !ok1 {
-        // proper EOF
-        return nil, io.EOF
-    }
+	t1, ok1 := p.popToken()
+	if !ok1 {
+		// proper EOF
+		return nil, io.EOF
+	}
 
-    t2, ok2 := p.peekToken()
-    switch strings.ToLower(t1) {
-    case "\n":
-        // Don't consume t2 and continue parsing
-        return nil, nil
-    case "user-agent":
-        if !ok2 {
-            // TODO: report error
-            return nil, errors.New("Unexpected EOF at token #" + strconv.Itoa(p.pos) + " namely: \"" + t1 + "\"")
-        }
-        p.agent = t2
-        p.popToken()
-        // continue parsing
-        return nil, nil
-    case "disallow":
-        if p.agent == "" {
-            // TODO: report error
-            return nil, errors.New("Disallow before User-agent.")
-        }
-        p.popToken()
+	t2, ok2 := p.peekToken()
+	switch strings.ToLower(t1) {
+	case "\n":
+		// Don't consume t2 and continue parsing
+		return nil, nil
 
-        if t2 == "" {
-          return &Rule{Agent: p.agent, Uri: t2, Allow: true}, nil
-        } else {
-          return &Rule{Agent: p.agent, Uri: t2, Allow: false}, nil
-        }
+	case "user-agent", "useragent":
+		// From google's spec:
+		// Handling of <field> elements with simple errors / typos (eg "useragent"
+		// instead of "user-agent") is undefined and may be interpreted as correct
+		// directives by some user-agents.
+		if !ok2 {
+			// TODO: report error
+			return nil, errors.New("Unexpected EOF at token #" + strconv.Itoa(p.pos) + " namely: \"" + t1 + "\"")
+		}
+		p.agent = t2
+		p.popToken()
+		// continue parsing
+		return nil, nil
 
-    case "allow":
-        if p.agent == "" {
-            // TODO: report error
-            return nil, errors.New("Disallow before User-agent.")
-        }
-        p.popToken()
-        return &Rule{Agent: p.agent, Uri: t2, Allow: true}, nil
-    }
+	case "disallow":
+		if p.agent == "" {
+			// TODO: report error
+			return nil, errors.New("Disallow before User-agent.")
+		}
+		p.popToken()
 
-    return nil, errors.New("Unknown token: " + strconv.Quote(t1))
+		// From google's spec:
+		// When no path is specified, the directive is ignored.
+		if t2 != "" {
+			return &Rule{Agent: p.agent, Uri: t2, Allow: false}, nil
+		} else {
+			return nil, nil
+		}
+
+	case "allow":
+		if p.agent == "" {
+			// TODO: report error
+			return nil, errors.New("Allow before User-agent.")
+		}
+		p.popToken()
+		// From google's spec:
+		// When no path is specified, the directive is ignored.
+		if t2 != "" {
+			return &Rule{Agent: p.agent, Uri: t2, Allow: true}, nil
+		} else {
+			return nil, nil
+		}
+
+	case "sitemap":
+		// Non-group field, applies to the host as a whole, not to a specific user-agent
+		if t2 != "" {
+			p.sitemaps = append(p.sitemaps, t2)
+		}
+		p.popToken()
+		return nil, nil
+
+	case "crawl-delay", "crawldelay":
+		// From http://en.wikipedia.org/wiki/Robots_exclusion_standard#Nonstandard_extensions
+		// Several major crawlers support a Crawl-delay parameter, set to the
+		// number of seconds to wait between successive requests to the same server.
+		if p.agent == "" {
+			return nil, errors.New("Crawl-delay before User-agent.")
+		}
+		p.popToken()
+		// TODO : Continue here with crawl-delay...
+	}
+
+	return nil, errors.New("Unknown token: " + strconv.Quote(t1))
 }
 
 func (p *Parser) popToken() (tok string, ok bool) {
-    if p.pos >= len(p.tokens) {
-        return "", false
-    }
-    tok = p.tokens[p.pos]
-    p.pos++
-    return tok, true
+	if p.pos >= len(p.tokens) {
+		return "", false
+	}
+	tok = p.tokens[p.pos]
+	p.pos++
+	return tok, true
 }
 
 func (p *Parser) peekToken() (tok string, ok bool) {
-    if p.pos >= len(p.tokens) {
-        return "", false
-    }
-    return p.tokens[p.pos], true
+	if p.pos >= len(p.tokens) {
+		return "", false
+	}
+	return p.tokens[p.pos], true
 }
--- robotstxt.go
+++ robotstxt.go
@@ -4,122 +4,135 @@
 package robotstxt
 
 import (
-    "bytes"
-    "errors"
-    "strings"
+	"bytes"
+	"errors"
+	"strings"
 )
 
 type RobotsData struct {
-    DefaultAgent string
-    // private
-    rules       []Rule
-    allowAll    bool
-    disallowAll bool
+	DefaultAgent string
+	// private
+	groups      []group
+	allowAll    bool
+	disallowAll bool
+	sitemaps    []string
 }
 
-type Rule struct {
-    Agent string
-    Uri   string
-    Allow bool
+type group struct {
+	agent      string
+	rules      []rule
+	crawlDelay uint
 }
 
-var AllowAll = &RobotsData{allowAll: true}
-var DisallowAll = &RobotsData{disallowAll: true}
+type rule struct {
+	path  string
+	allow bool
+}
+
+var allowAll = &RobotsData{allowAll: true}
+var disallowAll = &RobotsData{disallowAll: true}
 
 func FromResponseBytes(statusCode int, body []byte, print_errors bool) (*RobotsData, error) {
-    switch {
-    case statusCode == 404:
-        return AllowAll, nil
-    case statusCode == 401 || statusCode == 403:
-        return DisallowAll, nil
-    case statusCode >= 200 && statusCode < 300:
-        return FromBytes(body, print_errors)
-    }
-    // Conservative disallow all default
-    return DisallowAll, nil
+	switch {
+	//
+	// From https://developers.google.com/webmasters/control-crawl-index/docs/robots_txt
+	//
+	// Google treats all 4xx errors in the same way and assumes that no valid
+	// robots.txt file exists. It is assumed that there are no restrictions.
+	// This is a "full allow" for crawling. Note: this includes 401
+	// "Unauthorized" and 403 "Forbidden" HTTP result codes.
+	case statusCode >= 400 && statusCode < 500:
+		return AllowAll, nil
+	case statusCode >= 200 && statusCode < 300:
+		return FromBytes(body, print_errors)
+	}
+	// Conservative disallow all default
+	//
+	// From https://developers.google.com/webmasters/control-crawl-index/docs/robots_txt
+	//
+	// Server errors (5xx) are seen as temporary errors that result in a "full
+	// disallow" of crawling.
+	return DisallowAll, nil
 }
 
 func FromResponse(statusCode int, body string, print_errors bool) (*RobotsData, error) {
-    return FromResponseBytes(statusCode, []byte(body), print_errors)
+	return FromResponseBytes(statusCode, []byte(body), print_errors)
 }
 
 func FromBytes(body []byte, print_errors bool) (r *RobotsData, err error) {
-    // special case (probably not worth optimization?)
-    trimmed := bytes.TrimSpace(body)
-    if len(trimmed) == 0 {
-        return AllowAll, nil
-    }
+	// special case (probably not worth optimization?)
+	trimmed := bytes.TrimSpace(body)
+	if len(trimmed) == 0 {
+		return AllowAll, nil
+	}
 
-    sc := NewByteScanner("bytes", false)
-    sc.Quiet = !print_errors
-    sc.Feed(body, true)
-    var tokens []string
-    tokens, err = sc.ScanAll()
-    if err != nil {
-        return nil, err
-    }
+	sc := NewByteScanner("bytes", false)
+	sc.Quiet = !print_errors
+	sc.Feed(body, true)
+	var tokens []string
+	tokens, err = sc.ScanAll()
+	if err != nil {
+		return nil, err
+	}
 
-    // special case worth optimization
-    if len(tokens) == 0 {
-        return AllowAll, nil
-    }
+	// special case worth optimization
+	if len(tokens) == 0 {
+		return AllowAll, nil
+	}
 
-    r = &RobotsData{}
-    parser := NewParser(tokens)
-    r.rules, err = parser.ParseAll()
+	r = &RobotsData{}
+	parser := NewParser(tokens)
+	r.rules, err = parser.ParseAll()
 
-    return r, err
+	return r, err
 }
 
 func FromString(body string, print_errors bool) (r *RobotsData, err error) {
-    return FromBytes([]byte(body), print_errors)
+	return FromBytes([]byte(body), print_errors)
 }
 
-func (r *RobotsData) Test(url string) (bool, error) {
-    if r.DefaultAgent == "" {
-        return false, errors.New("DefaultAgent is empty. You MUST set RobotsData.DefaultAgent to use Test method.")
-    }
-    return r.TestAgent(url, r.DefaultAgent)
+func (r *RobotsData) Test(url string) bool {
+	return r.TestAgent(url, r.DefaultAgent)
 }
 
-func (r *RobotsData) TestAgent(url, agent string) (allow bool, err error) {
-    if r.allowAll {
-        return true, nil
-    }
-    if r.disallowAll {
-        return false, nil
-    }
+func (r *RobotsData) TestAgent(url, agent string) (allow bool) {
+	if r.allowAll {
+		return true
+	}
+	if r.disallowAll {
+		return false
+	}
 
-    // optimistic
-    allow = true
-    for _, rule := range r.rules {
-        if rule.MatchAgent(agent) && rule.MatchUrl(url) {
-            allow = rule.Allow
-            // stop on first disallow as safety default
-            // in absense of better algorithm
-            if !rule.Allow {
-                break
-            }
-        }
-    }
+	// optimistic
+	allow = true
+	for _, rule := range r.rules {
+		if rule.MatchAgent(agent) && rule.MatchUrl(url) {
+			allow = rule.Allow
+			// stop on first disallow as safety default
+			// in absense of better algorithm
+			if !rule.Allow {
+				break
+			}
+		}
+	}
 
-    return allow, nil
+	return allow
 }
 
 func (rule *Rule) MatchAgent(agent string) bool {
-    l_agent := strings.ToLower(agent)
-    l_rule_agent := strings.ToLower(rule.Agent)
-    return rule.Agent == "*" || strings.HasPrefix(l_agent, l_rule_agent)
+	l_agent := strings.ToLower(agent)
+	l_rule_agent := strings.ToLower(rule.Agent)
+	return rule.Agent == "*" || strings.HasPrefix(l_agent, l_rule_agent)
 }
 
 func (rule *Rule) MatchUrl(url string) bool {
-    return strings.HasPrefix(url, rule.Uri)
+	return strings.HasPrefix(url, rule.Uri)
 }
 
 func (rule *Rule) String() string {
-    allow_str := "Disallow"
-    if rule.Allow {
-        allow_str = "Allow"
-    }
-    return "<" + allow_str + " " + rule.Agent + " " + rule.Uri + ">"
+	allow_str := "Disallow"
+	if rule.Allow {
+		allow_str = "Allow"
+	}
+	return "<" + allow_str + " " + rule.Agent + " " + rule.Uri + ">"
 }
