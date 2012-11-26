207003cc18bd0551c9699702da3eb507fd1e1009
Sergey Shepelev <temotor@gmail.com>
2012-11-26 19:47:13 +0400

    Command line tool to check robots.txt access

---
+++ robots.txt-check/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"github.com/temoto/robotstxt.go"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var checkPaths = []string{
+	"/",
+}
+
+func main() {
+	robotsUrl := flag.String("robots-url", "", "")
+	bot := flag.String("bot", "GoogleBot", "")
+	flag.Parse()
+	if *robotsUrl == "" {
+		log.Fatalln("Robots URL is empty, run with -h to see usage.")
+	}
+	if !strings.HasPrefix(*robotsUrl, "http") {
+		*robotsUrl = "http://" + *robotsUrl
+	}
+
+	response, err := http.Get(*robotsUrl)
+	if err != nil {
+		log.Fatalln("HTTP error:", err)
+	}
+
+	robots, err := robotstxt.FromResponse(response)
+	if err != nil {
+		log.Fatalln("Robots.txt error:", err)
+	}
+
+	log.Println("Running checks as", *bot)
+	group := robots.FindGroup(*bot)
+	for _, path := range checkPaths {
+		log.Println(path, ":", group.Test(path))
+	}
+}
