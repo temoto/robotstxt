a78d78fbc50683082869ab36048f136b5d4d1be7
Gary Powell <gpowell@ezoic.com>
2021-10-07 10:35:46 -0700

    ran go fmt

--- robotstxt.go
+++ robotstxt.go
@@ -226,17 +226,17 @@ func (r *RobotsData) SetGroups(groups map[string]*Group) {
 	r.groups = groups
 }
 
-func(r *RobotsData) MarshalJSON() ([]byte, error) {
+func (r *RobotsData) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
-		"allow_all":r.allowAll,
-		"disallow_all":r.disallowAll,
-		"groups":r.groups,
-		"host":r.Host,
-		"sitemaps":r.Sitemaps,
+		"allow_all":    r.allowAll,
+		"disallow_all": r.disallowAll,
+		"groups":       r.groups,
+		"host":         r.Host,
+		"sitemaps":     r.Sitemaps,
 	})
 }
 
-func(r *RobotsData) UnmarshalJSON(bytes []byte) error {
+func (r *RobotsData) UnmarshalJSON(bytes []byte) error {
 	var robotsDataInterface map[string]interface{}
 	err := json.Unmarshal(bytes, &robotsDataInterface)
 
@@ -388,15 +388,15 @@ func (g *Group) findRule(path string) (ret *rule) {
 	return
 }
 
-func(g *Group) MarshalJSON() ([]byte, error) {
+func (g *Group) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
-		"agent":g.Agent,
-		"crawl_delay":g.CrawlDelay.Nanoseconds(),
-		"rules":g.rules,
+		"agent":       g.Agent,
+		"crawl_delay": g.CrawlDelay.Nanoseconds(),
+		"rules":       g.rules,
 	})
 }
 
-func(r *rule) MarshalJSON() ([]byte, error) {
+func (r *rule) MarshalJSON() ([]byte, error) {
 	var pattern string
 
 	if r.pattern != nil {
@@ -404,9 +404,8 @@ func(r *rule) MarshalJSON() ([]byte, error) {
 	}
 
 	return json.Marshal(map[string]interface{}{
-		"allow":r.allow,
-		"path":r.path,
-		"pattern":pattern,
+		"allow":   r.allow,
+		"path":    r.path,
+		"pattern": pattern,
 	})
 }
-
