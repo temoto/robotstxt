b39e3c75a13dbcebe7ce1ceee7017c0ef8a2a56d
Gary Powell <gpowell@ezoic.com>
2022-09-01 15:46:39 -0700

    add another test case.

--- robotstxt_test.go
+++ robotstxt_test.go
@@ -219,6 +219,16 @@ Sitemap:
 	expectAllAgents(t, r, true, "/")
 }
 
+// Test with misspelling of user-agent
+func TestFromString11(t *testing.T){
+	t.Parallel()
+	r, err := FromString(`
+user-agent: Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36 (compatible; EzLynx/0.1; +http://www.ezoic.com/bot.html)
+Allow: /`)
+	require.NoError(t, err)
+	expectAccess(t, r, false, "/", "Mozilla/5.0 (Linux; Android 8.0; Pixel 2 Build/OPD3.170816.012) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Mobile Safari/537.36 (compatible; EzLynx/0.1; +http://www.ezoic.com/bot.html)")
+}
+
 func TestInvalidEncoding(t *testing.T) {
 	// Invalid UTF-8 encoding should not break parser.
 	_, err := FromString("User-agent: H\xef\xbf\xbdm�h�kki\nDisallow: *")
@@ -283,6 +293,16 @@ func TestHost(t *testing.T) {
 			assert.Equal(t, c.expect, r.Host)
 		})
 	}
+}
+
+func TestChemicalaid(t *testing.T) {
+	const robotsChemicalaid = ("# Tier 0\nUser-agent: Googlebot\nUser-agent: Googlebot-Mobile\nUser-agent: Yandex # Russia\nDisallow: /admin/\nAllow: /\n#Crawl-delay: 1\n\n# Tier 1\nUser-agent: Bingbot # Bing\nUser-agent: Slurp # Yahoo\nUser-agent: coccoc # Vietnam\nUser-agent: DuckDuckBot # DuckDuckGo\nUser-agent: baiduspider # China\nUser-agent: Applebot # Apple\nDisallow: /admin/\nAllow: /\n#Crawl-delay: 10\n\n# Tier 1.5\nUser-agent: SeznamBot # Czech Republic\nUser-agent: NaverBot # South Korea\nUser-agent: Yeti # South Korea\nUser-agent: Mail.ru # Russia\nUser-agent: Sogou # China\nUser-agent: 360Spider # China\nDisallow: /admin/\nAllow: /\n#Crawl-delay: 30\n\n# Tier 2\nUser-agent: Teoma # Ask\n" +
+		"User-agent: SputnikBot # Russia\nUser-agent: Daumoa # South Korea\nUser-agent: Dazoobot # France\nUser-agent: DeuSu # Germany\nUser-agent: EuripBot # Europe\nUser-agent: Exploratodo # Latin America\nUser-agent: istellabot # Italy\nUser-agent: moget # Japan\nUser-agent: ichiro # Japan\nUser-agent: Petalbot # Huawei\nUser-agent: Amazonbot\nUser-agent: Neevabot\nDisallow: /admin/\nAllow: /\n#Crawl-delay: 60\nCrawl-delay: 15\n\n# Tier 2.5\nUser-agent: Googlebot-News\nUser-agent: Googlebot-Video\nUser-agent: Googlebot-Image\nUser-agent: AdsBot-Google\nUser-agent: BingPreview\nUser-agent: msnbot\nUser-agent: msnbot-media\nDisallow: /admin/\nAllow: /\n#Crawl-delay: 90\n\n# Tier 3\n#User-agent: ccbot # commoncrawl.org\n#Disallow: /admin/\n#Allow: /\n#Crawl-delay: 120\n\n# Ads\nUser-agent: Mediapartners-Google\nUser-agent: ias_crawler\nUser-agent: GrapeshotCrawler\nUser-agent: CriteoBot\nUser-agent: TTD-Content\nUser-agent: weborama-fetcher\nUser-agent: AmazonAdBot\nDisallow: /admin/\nAllow: /\n\n# Blocked\nUser-agent: AhrefsBot\nUser-agent: SemrushBot\nUser-agent: BLEXBot\nUser-agent: EzoicBot\nDisallow: /\n\nUser-agent: *\nDisallow: /\n\nSitemap: https://www.chemicalaid.com/sitemap.php")
+
+	t.Parallel()
+	r, err := FromString(robotsChemicalaid)
+	require.NoError(t, err)
+	expectAccess(t, r, false, "/", "EzoicBot")
 }
 
 /*
@@ -407,6 +427,11 @@ func expectAll(t *testing.T, r *RobotsData, allow bool) {
 	expectAllAgents(t, r, allow, "/admin/")
 	expectAllAgents(t, r, allow, "/search")
 	expectAllAgents(t, r, allow, "/.htaccess")
+}
+
+func expectNoAccess(t *testing.T, r *RobotsData, allow bool) {
+	// TODO fuzz path
+	expectAllAgents(t, r, allow, "/")
 }
 
 func expectAllAgents(t *testing.T, r *RobotsData, allow bool, path string) {
